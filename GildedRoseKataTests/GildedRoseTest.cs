53a4052b4acae0a71c1d25250c6fec74cfe54094
djm256 <daniel.j.miller@sherwin.com>
2016-05-23 13:25:44 -0400

    Added blank kata

---
+++ GildedRoseKataTests/GildedRoseTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace GildedRoseKataTests
+{
+	[TestFixture]
+	public class GildedRoseTest
+	{
+		[Test]
+		public void True_Is_True() {
+			Assert.IsTrue(true);
+		}
+	}
+}
+
